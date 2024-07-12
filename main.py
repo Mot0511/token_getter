922e3290b5a2311d694acd616824a7308b7efcae
Mot0511 <suvorov.matvej9@gmail.com>
2024-07-12 09:39:26 +0300

    _

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def main(data: RequestModel):
     url = 'https://passport.43edu.ru/auth/login'
     data = {'login': data.login, 'password': data.password, "submit": "submit", "returnTo": "https://one.43edu.ru"}
     
-    session.post(url, data=data, cert=('/cert.pem', '/key.pem'))
+    session.post(url, data=data, verify='cert.pem')
 
     token = next((i for i in session.cookies if i.name == 'X1_SSO'), None)
 
