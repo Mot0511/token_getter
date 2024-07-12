ed032577df8216790b1392ec2f33cb55a48e371d
Mot0511 <suvorov.matvej9@gmail.com>
2024-07-12 09:12:53 +0300

    _

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def main(data: RequestModel):
 
 if __name__ == '__main__':
     uvicorn.run("main:app",
-            host="127.0.0.1",
+            host="0.0.0.0",
             port=8000,
             reload=True,
             ssl_keyfile="./key.pem", 
